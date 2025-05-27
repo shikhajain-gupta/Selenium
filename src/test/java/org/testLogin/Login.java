091c79de50b1aa7882eb6d5e66b0b813a5e85aeb
shikhajain-gupta <shikhajain.gupta@gmail.com>
2025-05-27 10:34:13 +0400

    co&&11

---
+++ src/test/java/org/testLogin/Login.java
@@ -0,0 +1,31 @@
+package org.testLogin;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Login {
+    public static void main(String[] args)  {
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://practicetestautomation.com/practice-test-login/");
+        WebElement username = driver.findElement(By.id("username"));
+        username.sendKeys("student");
+        WebElement password = driver.findElement(By.id("password"));
+        password.sendKeys("Password123");
+        WebElement loginButton = driver.findElement(By.id("submit"));
+        loginButton.click();
+
+       try {
+           Thread.sleep(500);
+       }
+       catch (InterruptedException e) {
+           System.out.println(e);
+       }
+
+
+        driver.quit();
+    }
+}
