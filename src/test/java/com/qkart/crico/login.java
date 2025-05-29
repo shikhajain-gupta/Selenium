b9990485d25517bbb4e0b1424450378fa07c74b7
shikhajain-gupta <shikhajain.gupta@gmail.com>
2025-05-29 17:18:15 +0400

    commit2905

---
+++ src/test/java/com/qkart/crico/login.java
@@ -0,0 +1,72 @@
+package com.qkart.crico;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+public class login {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://crio-qkart-frontend-qa.vercel.app/");
+        Thread.sleep(3000);
+
+
+WebElement register = driver.findElement(By.xpath("//div[@class='css-vb6e92']/button[text()='Register']"));
+
+        //div/div/div[1]/div[3]/button[2]
+        //div[@class='css-vb6e92']/child::button[text()='Register']
+        //div[@class='css-vb6e92']/button[text()='Register']
+
+        register.click();
+        Thread.sleep(3000);
+
+        driver.findElement(By.id("username")).sendKeys("shikhatest3");
+        Thread.sleep(1000);
+        driver.findElement(By.id("password")).sendKeys("shikhatest3");
+        Thread.sleep(1000);
+        driver.findElement(By.id("confirmPassword")).sendKeys("shikhatest3");
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//div[@class='form css-ikzlcq']/button[1]")).click();
+        //div[@class='form css-ikzlcq']/button[1]
+        Thread.sleep(3000);
+
+        String url =driver.getCurrentUrl();
+        if(url.contains("login"))
+        {
+            System.out.println("You are registered successfully");
+            driver.findElement(By.id("username")).sendKeys("shikhatest3");
+            Thread.sleep(1000);
+            driver.findElement(By.id("password")).sendKeys("shikhatest3");
+            Thread.sleep(1000);
+            driver.findElement(By.xpath("//div[@class='form css-ikzlcq']/button")).click();
+            //div[@class='form css-ikzlcq']/button
+            Thread.sleep(3000);
+            String name = driver.findElement(By.xpath("//div[@class='css-vb6e92']/p")).getText();
+            System.out.println(name + " is logged in");
+
+
+            //to count images on page
+            List<WebElement> images = driver.findElements(By.cssSelector(".MuiCardMedia-root.MuiCardMedia-media.MuiCardMedia-img.css-rhsghg"));
+
+            System.out.println("total no of images on this page are :" + images.size());
+
+            //to print product names in console
+            //List<WebElement> imageText = driver.findElements(By.className("MuiTypography-root MuiTypography-body1 css-yg30e6"));
+            List<WebElement> imageText = driver.findElements(By.cssSelector(".MuiTypography-root.MuiTypography-body1.css-yg30e6"));
+            for(WebElement img : imageText)
+            {
+                System.out.println(img.getText());
+            }
+
+            driver.quit();
+        }
+        else {
+            System.out.println("Login first");
+        }
+
+    }
+}
