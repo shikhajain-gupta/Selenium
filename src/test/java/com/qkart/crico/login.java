8240cc554a7150372c5990b992da4486c48a9a9e
shikhajain-gupta <shikhajain.gupta@gmail.com>
2025-05-30 09:04:43 +0400

    commit30051

--- src/test/java/com/qkart/crico/login.java
+++ src/test/java/com/qkart/crico/login.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class login {
@@ -15,7 +16,7 @@ public class login {
         Thread.sleep(3000);
 
 
-WebElement register = driver.findElement(By.xpath("//div[@class='css-vb6e92']/button[text()='Register']"));
+        WebElement register = driver.findElement(By.xpath("//div[@class='css-vb6e92']/button[text()='Register']"));
 
         //div/div/div[1]/div[3]/button[2]
         //div[@class='css-vb6e92']/child::button[text()='Register']
@@ -24,47 +25,50 @@ WebElement register = driver.findElement(By.xpath("//div[@class='css-vb6e92']/bu
         register.click();
         Thread.sleep(3000);
 
-        driver.findElement(By.id("username")).sendKeys("shikhatest3");
+        driver.findElement(By.id("username")).sendKeys("shikhatest4");
         Thread.sleep(1000);
-        driver.findElement(By.id("password")).sendKeys("shikhatest3");
+        driver.findElement(By.id("password")).sendKeys("shikhatest4");
         Thread.sleep(1000);
-        driver.findElement(By.id("confirmPassword")).sendKeys("shikhatest3");
+        driver.findElement(By.id("confirmPassword")).sendKeys("shikhatest4");
         Thread.sleep(1000);
         driver.findElement(By.xpath("//div[@class='form css-ikzlcq']/button[1]")).click();
         //div[@class='form css-ikzlcq']/button[1]
         Thread.sleep(3000);
 
-        String url =driver.getCurrentUrl();
-        if(url.contains("login"))
-        {
+        String url = driver.getCurrentUrl();
+        if (url.contains("login")) {
             System.out.println("You are registered successfully");
-            driver.findElement(By.id("username")).sendKeys("shikhatest3");
+            driver.findElement(By.id("username")).sendKeys("shikhatest4");
             Thread.sleep(1000);
-            driver.findElement(By.id("password")).sendKeys("shikhatest3");
+            driver.findElement(By.id("password")).sendKeys("shikhatest4");
             Thread.sleep(1000);
             driver.findElement(By.xpath("//div[@class='form css-ikzlcq']/button")).click();
-            //div[@class='form css-ikzlcq']/button
             Thread.sleep(3000);
             String name = driver.findElement(By.xpath("//div[@class='css-vb6e92']/p")).getText();
             System.out.println(name + " is logged in");
 
 
             //to count images on page
-            List<WebElement> images = driver.findElements(By.cssSelector(".MuiCardMedia-root.MuiCardMedia-media.MuiCardMedia-img.css-rhsghg"));
 
+            //List<WebElement> images = driver.findElements(By.cssSelector(".MuiCardMedia-root.MuiCardMedia-media.MuiCardMedia-img.css-rhsghg"));
+            List<WebElement> images = driver.findElements(By.xpath("//div[@class='App']/descendant::img"));
             System.out.println("total no of images on this page are :" + images.size());
 
             //to print product names in console
-            //List<WebElement> imageText = driver.findElements(By.className("MuiTypography-root MuiTypography-body1 css-yg30e6"));
-            List<WebElement> imageText = driver.findElements(By.cssSelector(".MuiTypography-root.MuiTypography-body1.css-yg30e6"));
-            for(WebElement img : imageText)
-            {
-                System.out.println(img.getText());
+
+            //List<WebElement> imageText = driver.findElements(By.cssSelector(".MuiTypography-root.MuiTypography-body1.css-yg30e6"));
+            for (WebElement imageText : images) {
+                String text = imageText.getAttribute("alt");
+                System.out.println("image text is " + text);
             }
 
+            //for(WebElement img : imageText)
+            //{
+            //  System.out.println(img.getText());
+            //}
+
             driver.quit();
-        }
-        else {
+        } else {
             System.out.println("Login first");
         }
 
