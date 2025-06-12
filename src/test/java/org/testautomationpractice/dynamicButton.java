b9896527ecc97fd61445e840bc1e1d9c9ad180c7
shikhajain-gupta <shikhajain.gupta@gmail.com>
2025-06-12 10:50:49 +0400

    commit061201

--- src/test/java/org/testautomationpractice/dynamicButton.java
+++ src/test/java/org/testautomationpractice/dynamicButton.java
@@ -3,18 +3,26 @@ package org.testautomationpractice;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-
+import org.testng.annotations.Test;
 
 
 public class dynamicButton {
 
-    Alert1 obj= new Alert1();
-    WebDriver driver = obj.chrome();
-    WebElement dbutton = driver.findElement(By.xpath("//div[@class='widget-content']/button[@onclick='toggleButton(this)']"));
-    String buttonText = dbutton.getText();
-    System.out.println(buttonText);
-    dbutton.click();
-    System.out.println(buttonText);
-    driver.quit();
+    @Test
+    public void dynamicButton() {
+        Alert1 obj = new Alert1();
+        WebDriver driver = obj.chrome();
+        WebElement dbutton = driver.findElement(By.xpath("//div[@class='widget-content']/button[@onclick='toggleButton(this)']"));
+        //String buttonText = dbutton.getText();
 
+        for(int i=0; i<=5;i++) {
+            System.out.println(i+1+ "iteration");
+            System.out.println(dbutton.getText());
+            dbutton.click();
+            System.out.println(dbutton.getText());
+            dbutton.click();
+        }
+        driver.quit();
+
+    }
 }
